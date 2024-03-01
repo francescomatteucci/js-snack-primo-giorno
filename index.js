1ee270999ff7098554eff71444624bafdde1ef87
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-01 13:12:09 +0100

    ADD: JS-snack

---
+++ index.js
@@ -0,0 +1,14 @@
+let number = 100 //number
+console.log (number)
+
+const PI = 3.14 //number
+console.log (PI)
+//Essendo PI una costante non gli puoi cambiare il valore
+let radius = 8 //number
+let circle = radius * 2 //number
+console.log (circle)
+
+let name = "Carlo" //string
+console.log (name)
+name = "Marco" //string
+console.log (name)
