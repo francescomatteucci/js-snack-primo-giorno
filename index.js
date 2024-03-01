9080a3b8a810da6ee094c7bbe392d91871a40712
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-01 13:28:56 +0100

    ADD circle area

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PI = 3.14 //number
 console.log (PI)
 //Essendo PI una costante non gli puoi cambiare il valore
 let radius = 8 //number
-let circle = radius * 2 //number
+let circle = radius * 2 * PI //number
 console.log (circle)
 
 let name = "Carlo" //string
